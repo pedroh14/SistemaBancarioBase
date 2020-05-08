605d2f2a6db34c05650c03d91ebe5bbda151a3d3
pedroh14 <50341791+pedroh14@users.noreply.github.com>
2020-05-08 11:54:41 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package codigo;
+
+import java.text.ParseException;
+
+public class main {
+
+    public static void main(String[] args) throws ParseException {
+        System.out.print("funcionario ");
+
+
+
+    }
+}
+
+
+# Em desenvolvimento
